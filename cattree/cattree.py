67e5e886a181fdab3b66a73a315b07958438318f
Pablo Skewes <pablo.skewes@ug.uchile.cl>
2025-01-08 03:20:21 -0300

    add path name on top of file formatting

--- cattree/cattree.py
+++ cattree/cattree.py
@@ -101,12 +101,17 @@ def traverse_directory_dfs(
                 stack.append((entry, depth + 1))
 
 
-def format_file_content(path: Path, max_lines: Optional[int] = None) -> str:
+def format_file_content(
+    path: Path,
+    root_path: Path,
+    max_lines: Optional[int] = None,
+) -> str:
     """
     Read the content of a file up to a specified number of lines.
 
     Args:
         path (Path): Path to the file.
+        root_path (Path): Root path to use for relative paths.
         max_lines (Optional[int]): Maximum number of lines to read.
             If None, read the entire file.
 
@@ -116,7 +121,7 @@ def format_file_content(path: Path, max_lines: Optional[int] = None) -> str:
     if not path.is_file():
         raise ValueError(f"The path {path} is not a valid file.")
 
-    lines = [str(path)]
+    lines = [f"{path.relative_to(root_path)}:\n"]
     try:
         with open(path, "r", encoding="utf-8") as file:
             for i, line in enumerate(file):
@@ -167,12 +172,14 @@ def generate_cattree(
         tree_structure.append(f"{prefix}{connector}{entry.path.name}")
 
         if entry.path.is_file():
-            file_contents.append(format_file_content(entry.path))
+            file_contents.append(
+                format_file_content(path=entry.path, root_path=directory)
+            )
         else:
             file_contents.append("")  # Empty line for directories
 
     formatted_directory_tree = "\n".join(tree_structure)
-    formatted_file_contents = f"\n{'-' * 80}\n".join(
+    formatted_file_contents = f"{'-' * 80}\n".join(
         (content for content in file_contents if content)
     )
     return f"{formatted_directory_tree}\n{formatted_file_contents}"
@@ -200,11 +207,11 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    root_path = Path(args.path)
+    _root_path = Path(args.path)
     try:
         print(
             generate_cattree(
-                root_path,
+                _root_path,
                 include_pattern=args.include_pattern,
                 exclude_pattern=args.exclude_pattern,
             )
